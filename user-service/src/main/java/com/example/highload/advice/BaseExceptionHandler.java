92b2bf441d5264ec2aa669ec808102f2e1b8f1ad
abogatov <abogatov@solanteq.com>
2024-01-08 10:12:38 +0300

    fix: remove incorrect packaging in user service

--- user-service/src/main/java/com/example/highload/advice/BaseExceptionHandler.java
+++ user-service/src/main/java/com/example/highload/advice/BaseExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.highload.advice;
 
-import com.example.highload.exception.base.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
@@ -9,7 +8,6 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
--- user-service/src/main/java/com/example/highload/repos/RoleRepository.java
+++ user-service/src/main/java/com/example/highload/repos/RoleRepository.java
@@ -2,7 +2,6 @@ package com.example.highload.repos;
 
 import com.example.highload.model.enums.RoleType;
 import com.example.highload.model.inner.Role;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
