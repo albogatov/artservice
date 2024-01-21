55536ed6b97057b480662bfb5059ea2b221c1137
abogatov <abogatov@solanteq.com>
2024-01-21 15:14:35 +0300

    feat: websocket access through gateway

--- notification-service/src/main/java/com/example/highload/notification/websocket/WebSocketConfig.java
+++ notification-service/src/main/java/com/example/highload/notification/websocket/WebSocketConfig.java
@@ -3,12 +3,14 @@ package com.example.highload.notification.websocket;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Controller
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
